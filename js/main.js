656e1a10cfc919ee2a15f3fb0df59d8f616a5ff5
Takenouchi Keita <t.keita.osaka@gmail.com>
2019-03-04 00:58:19 +0900

    Highlighting of a current cell.

--- js/main.js
+++ js/main.js
@@ -140,14 +140,15 @@ $(".cell").on("click", function () {
 })
 
 $(".cell").mouseover(function () {
+    clearTableColor()
     if (targetCell) {
-        clearTableColor()
         setCellColorByXY(targetCell[0], targetCell[1], "yellow")
 
         var x = dom2Cell(this)[0]
         var y = dom2Cell(this)[1]
         highlightLine(targetCell[0], targetCell[1], x, y)
     }
+    setCellColorByDom(this, "yellow")
 })
 
 //     background-image: linear-gradient(to top right,red 2%,transparent 2%, transparent 48%, red 48%, red 52%, transparent 52%, transparent 98%, red 98%)

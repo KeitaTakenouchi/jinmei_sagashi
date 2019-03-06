19f1636c2b06b8fb6abb74b1c3f344ce23f0efcc
Takenouchi Keita <t.keita.osaka@gmail.com>
2019-03-06 22:35:13 +0900

    Added the jinmei data file.

---
+++ js/data.js
@@ -0,0 +1,77 @@
+var jinmei = [
+    {
+        "name": "いとうひろぶみ",
+        "display_name": "伊藤博文",
+        "message": "第1次伊藤内閣",
+    },
+    {
+        "name": "くろだきよたか",
+        "display_name": "黒田清隆",
+        "message": "黒田内閣",
+    },
+    {
+        "name": "さんじょうさねとみ",
+        "display_name": "三条実美",
+        "message": "三條暫定内閣",
+    },
+    {
+        "name": "やまがたありとも",
+        "display_name": "山縣有朋",
+        "message": "第1次山縣内閣",
+    },
+    {
+        "name": "まつかたまさよし",
+        "display_name": "松方正義",
+        "message": "第1次松方内閣",
+    },
+    {
+        "name": "おおくましげのぶ",
+        "display_name": "大隈重信",
+        "message": "第1次大隈内閣",
+    },
+    {
+        "name": "かつらたろう",
+        "display_name": "桂太郎",
+        "message": "第1次桂内閣",
+    },
+    {
+        "name": "さいおんじきんもち",
+        "display_name": "西園寺公望",
+        "message": "第1次西園寺内閣",
+    },
+    {
+        "name": "やまもとごんのひょうえ",
+        "display_name": "山本権兵衛",
+        "message": "第1次山本内閣",
+    },
+    {
+        "name": "てらうちまさたけ",
+        "display_name": "寺内正毅",
+        "message": "寺内内閣",
+    },
+    {
+        "name": "はらたかし",
+        "display_name": "原敬",
+        "message": "原内閣",
+    },
+    {
+        "name": "うちだこうさい",
+        "display_name": "内田康哉",
+        "message": "原内閣",
+    },
+    {
+        "name": "たかはしこれきよ",
+        "display_name": "高橋是清",
+        "message": "高橋内閣",
+    },
+    {
+        "name": "かとうともさぶろう",
+        "display_name": "加藤友三郎",
+        "message": "加藤友三郎内閣",
+    },
+    {
+        "name": "きようらけいご",
+        "display_name": "清浦奎吾",
+        "message": "清浦内閣",
+    },
+]
